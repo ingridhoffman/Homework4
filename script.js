8cc0befe04750e06f8c6b9181d132bc7a1a0006f
trapezechic <ingridhoffman@me.com>
2020-02-11 17:58:24 -0800

    html framework and script outline

---
+++ script.js
@@ -0,0 +1,27 @@
+// start quiz
+
+// start score at zero
+
+// start timer
+
+// loop until time is up:
+
+// - provide question and answer choices
+
+// - get user answer
+
+// - correct or wrong?
+
+// - update score
+
+// end quiz
+
+// show score
+
+// log user initials
+
+// rank user in list of scores
+
+// show list of highscores
+
+// try again?
