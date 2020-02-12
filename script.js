08eab31c6ffcd50e09cd6c5c9a1f012bd68d2fb8
trapezechic <ingridhoffman@me.com>
2020-02-12 12:53:25 -0800

    go to next question after user answers

--- script.js
+++ script.js
@@ -14,12 +14,28 @@ var quiz = [
 		correct: 4
 	},
 	{
-		question: "next question",
+		question: "second question",
 		1: "23",
 		2: "67",
 		3: "90",
 		4: "56",
 		correct: 2
+	},
+	{
+		question: "third question",
+		1: "12",
+		2: "623",
+		3: "9345",
+		4: "456",
+		correct: 3
+	},
+	{
+		question: "fourth question",
+		1: "278",
+		2: "345",
+		3: "78",
+		4: "52",
+		correct: 1
 	}
 ];
 console.log(quiz);
@@ -28,43 +44,59 @@ console.log(quiz);
 var score = 0;
 console.log(score);
 
-// function to take quiz
-function takeQuiz() {
-	// start timer
-	// loop until time is up:
+// start at first question
+var currentQ = 0;
+console.log(currentQ);
+
+function askQuestion(i) {
 	// provide question and answer choices
-	$("#content").html("<h2>" + quiz[0].question + "</h2>");
-	for (i = 1; i <= 4; i++) {
-		console.log(i);
-		console.log(quiz[0][i]);
+	$("#content").html("<h2>" + quiz[i].question + "</h2>");
+	for (x = 1; x <= 4; x++) {
+		console.log(x);
+		console.log(quiz[i][x]);
 		var newButton = $("<input/>").attr({
 			type: "button",
 			class: "choices",
-			id: i,
-			value: quiz[0][i]
+			id: x,
+			value: quiz[i][x]
 		});
 		$("#content").append(newButton);
 	}
+}
+
+function checkAnswer(i, userAnswer) {
+	// correct or wrong?
+	if (userAnswer === quiz[i].correct) {
+		var isCorrect = "Correct!";
+		console.log(isCorrect);
+		// update score
+		score++;
+		console.log(score);
+	} else {
+		var isCorrect = "Wrong.";
+		console.log(isCorrect);
+		console.log(score);
+	}
+	$("#content").append("<h3>" + isCorrect + "</h3>");
+}
+
+// function to take quiz
+function takeQuiz() {
+	// start timer
+	// provide next question
+	console.log(quiz[currentQ].question);
+	askQuestion(currentQ);
 	// get user answer
 	$(".choices").on("click", function() {
 		var userAnswer = parseInt(this.id);
 		console.log(userAnswer);
-		console.log(typeof userAnswer);
-		console.log(typeof quiz[0].correct);
-		// correct or wrong?
-		if (userAnswer === quiz[0].correct) {
-			var isCorrect = "Correct!";
-			console.log(isCorrect);
-			// update score
-			score++;
-			console.log(score);
-		} else {
-			var isCorrect = "Wrong.";
-			console.log(isCorrect);
-			console.log(score);
-		}
-		$("#content").append("<h3>" + isCorrect + "</h3>");
+		//check answer
+		checkAnswer(currentQ, userAnswer);
+		// go to next question
+		currentQ++;
+		setTimeout(takeQuiz, 1000);
 	});
+
 	// end quiz
 }
 
